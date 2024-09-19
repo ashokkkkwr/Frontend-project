8983b3809f6084e65d3bcd9a97c4b25bf0eaa021
Your Name <you@example.com>
2024-09-19 17:03:24 +0545

    completeed redis from displaying chat

--- redis/app.js
+++ redis/app.js
@@ -16,14 +16,16 @@ redisClient.connect().catch(console.error);
 app.get("/photos", async (req, res) => {
   const albumId = req.query.albumId;
   try {
-    const photos = await redisClient.get("photos");
+    const photos = await redisClient.get("photos")
     if (photos != null) {
+      console.log("from redius")
       return res.json(JSON.parse(photos));
     } else {
+      console.log("from endPoint")
       const { data } = await axios.get(
         "https://jsonplaceholder.typicode.com/photos",
         { params: { albumId } }
-      );
+      )
       await redisClient.setEx(
         "photos",
         DEFAULT_EXPIRATION,
